3e4342d15391a76880f3495d0fde7eef4d3a974d
Dushan Hadji-Vasilev <dushan.fsbrainster@gmail.com>
2024-12-13 06:50:20 +0100

    Project 01 attached.

---
+++ assets/JS/auto-typed.js
@@ -0,0 +1,10 @@
+// JS code for the Auto Typed Sentences that will be written on the Banner Section
+var typed = new Typed(".auto-typed", {
+  strings: [
+    "Brainster Labs",
+    "There are no limits to what you can accomplish, except the limits you place on your own thinking.",
+  ],
+  typeSpeed: 120,
+  backSpeed: 100,
+  loop: true,
+});
